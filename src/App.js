b7b8ef6005000f5fa70e7228935741245a32e341
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-19 10:42:01 +0100

    Finish display question

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
 function App() {
-  const { waiting, loading, questions } = useGlobalContext();
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
   if (waiting) {
     return <SetupForm />;
   }
   if (loading) {
     return <div className="loading"></div>;
   }
-  return <main>quiz questions</main>;
-  // const { correct_answer } = questions[0];
+  const { question, correct_answer, incorrect_answers } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+
+  return (
+    <main>
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
