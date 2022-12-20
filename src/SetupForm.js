88a0d9f97c3cc63dde7d4ef7636049a12b89348d
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-20 15:27:58 +0100

    Setup form structure

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -1,8 +1,59 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit } = useGlobalContext();
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              className="form-input"
+              min={1}
+              max={50}
+              value={quiz.amount}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">category</label>
+            <select
+              name="category"
+              id="category"
+              className="form-input"
+              onChange={handleChange}
+            >
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="difficulty">select difficulty</label>
+            <select
+              name="difficulty"
+              id="difficulty"
+              className="form-input"
+              onChange={handleChange}
+            >
+              <option value="easy">easy</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+          </div>
+          <button type="submit" className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
-export default SetupForm
+export default SetupForm;
